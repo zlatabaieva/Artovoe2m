f58aeaafbec0fb840896cb3348b8ddd4ba21f2f4
zlata baieva <zlata_baeva2004@icloud.com>
2024-11-06 14:57:26 +0300

    Initial Commit

---
+++ Tutorial/TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  TutorialApp.swift
+//  Tutorial
+//
+//  Created by Apple on 06.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
